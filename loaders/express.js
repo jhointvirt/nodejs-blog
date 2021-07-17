ac484356ce7c85f07bfda8c19d6ea30047571d2f
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-07-17 15:52:00 +0300

    fead: add some pages and routes

--- loaders/express.js
+++ loaders/express.js
@@ -1,7 +1,30 @@
+const path = require('path')
+const express = require('express')
+
 const expressLoader = async({app}) => {
+  // static files
+  app.use(express.static(path.join(__dirname, '../public')));
+  // set view engine
+  app.set('view engine', 'pug');
   // set main route
   app.get('/', (req, res, next)=>{
-    return res.send('Hello, world!')
+    return res.render('pages/index');
+  });
+  // GET about
+  app.get('/about', (req, res) => {
+    return res.render('pages/about')
+  });
+  // GET posts
+  app.get('/posts', (req, res) => {
+    return res.render('pages/posts')
+  });
+  // GET new-post
+  app.get('/new-post', (req, res) => {
+    return res.render('pages/new-post')
+  });
+  // GET post
+  app.get('/posts/:id', (req, res) => {
+    return res.render('pages/single-post')
   });
   return app;
 }
